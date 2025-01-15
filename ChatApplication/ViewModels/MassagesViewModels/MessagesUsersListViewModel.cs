850fbeab501ad1cc763e0339f429593fc1bff976
minanader2018@outlook.com <minanader2018@outlook.com>
2025-01-15 14:54:35 +0200

    ViewModel

---
+++ ChatApplication/ViewModels/MassagesViewModels/MessagesUsersListViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatApplication.ViewModels.MassagesViewModels
+{
+    public class MessagesUsersListViewModel
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string LastMessage { get; set; }
+    }
+}
---
+++ ChatApplication/ViewModels/MassagesViewModels/UserMessagesListViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ChatApplication.ViewModels.MassagesViewModels
+{
+    public class UserMessagesListViewModel
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public bool IsCurrentUserSentMessage { get; set; }
+    }
+}
